3eb461c3f99deae576810890a09e36ead5bd1ee6
Mehrdad Najafalizadeh <najafalizadehmehrdad@gmail.com>
2024-07-28 14:29:31 +0430

    --

--- Helpers/GoldUserInfoAttribute.cs
+++ Helpers/GoldUserInfoAttribute.cs
@@ -1,5 +1,8 @@
 ﻿using Accounting.Helpers;
+using Accounting.Models;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using RestSharp;
 
 namespace GoldHelpers.Helpers
 {
@@ -13,8 +16,39 @@ namespace GoldHelpers.Helpers
 
         public void OnActionExecuting(ActionExecutingContext context)
         {
+            string host = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build()
+                .GetSection("ProjectURLs")
+                .GetValue<string>("Accounting")!;
+
             UserInfoAttribute userInfoAttribute = new UserInfoAttribute();
             userInfoAttribute.OnActionExecuting(context);
+
+            try
+            {
+                // BaseURL
+                RestClient client = new($"{host}/api/SMTP/SendOTPSMS");
+                RestRequest request = new()
+                {
+                    Method = Method.Post
+                };
+
+                // Parameters
+                //request.AddJsonBody(new { Mobile = sms.Destination!.Value.ToString(), OTP = sms.Options!.Message });
+
+                // Headers
+                request.AddHeader("content-type", "application/json");
+                request.AddHeader("cache-control", "no-cache");
+
+                // Send SMS
+                RestResponse response = client.ExecutePost(request);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
